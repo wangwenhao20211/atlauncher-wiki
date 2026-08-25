9a326e8bf4ac3786976bfc4cfa73906005be32bf
wangwenhao20211 <159092574+wangwenhao20211@users.noreply.github.com>
2026-08-25 22:04:45 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,7 @@ export default defineConfig({
         starlight({
             title: 'Wwh Wiki',
             locales: {
-                'zh-cn': {
+                zh: {
                     label: '简体中文',
                     lang: 'zh-CN',
                 },
@@ -18,6 +18,7 @@ export default defineConfig({
                     lang: 'en',
                 },
             },
+            defaultLocale: 'zh',
             social: [
                 { icon: 'github', label: 'GitHub', href: 'https://github.com/wangwenhao20211/atlauncher-wiki' }
             ],
