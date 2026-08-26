72a3c6933a4471bb2191c5d623ea9047a947d990
wangwenhao20211 <159092574+wangwenhao20211@users.noreply.github.com>
2026-08-27 07:09:42 +0800

    更新 astro.config.ts

--- astro.config.ts
+++ astro.config.ts
@@ -2,59 +2,39 @@ import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 import starlightImageZoom from 'starlight-image-zoom';
 import starlightLinksValidatorPlugin from 'starlight-links-validator';
+import { sidebar } from './astro.sidebar';
 
 export default defineConfig({
-    integrations: [
-        starlight({
-            title: 'Wwh Wiki',
-            locales: {
-                root: {
-                    label: '简体中文',
-                    lang: 'zh-CN',
-                },
-                en: {
-                    label: 'English',
-                    lang: 'en',
-                },
-            },
-            defaultLocale: 'root',
-            sidebar: [
-                {
-                    label: '📚 全部文章',
-                    translations: {
-                        en: '📚 All Articles',
-                    },
-                    items: [
-                        {
-                            label: 'Wwhgames',
-                            translations: {
-                                en: 'Wwhgames',
-                            },
-                            autogenerate: { directory: 'wwhgames' },
-                            collapsed: true,
-                        },
-                        {
-                            label: 'B站UP',
-                            translations: {
-                                en: 'Bilibili Creators',
-                            },
-                            autogenerate: { directory: 'bilibili_up' },
-                            collapsed: true,
-                        },
-                    ],
-                },
-            ],
-            social: [
-                { icon: 'github', label: 'GitHub', href: 'https://github.com/wangwenhao20211/atlauncher-wiki' }
-            ],
-            pagination: false,
-            logo: {
-                src: './src/assets/logo.svg',
-            },
-            editLink: {
-                baseUrl: 'https://github.com/wangwenhao20211/atlauncher-wiki/edit/master',
-            },
-            plugins: [starlightImageZoom(), starlightLinksValidatorPlugin()],
-        }),
-    ],
+  integrations: [
+    starlight({
+      title: 'Wwh Wiki',
+      locales: {
+        root: {
+          label: '简体中文',
+          lang: 'zh-CN',
+        },
+        en: {
+          label: 'English',
+          lang: 'en',
+        },
+      },
+      defaultLocale: 'root',
+      sidebar,
+      social: [
+        {
+          icon: 'github',
+          label: 'GitHub',
+          href: 'https://github.com/wangwenhao20211/atlauncher-wiki',
+        },
+      ],
+      pagination: false,
+      logo: {
+        src: './src/assets/logo.svg',
+      },
+      editLink: {
+        baseUrl: 'https://github.com/wangwenhao20211/atlauncher-wiki/edit/master',
+      },
+      plugins: [starlightImageZoom(), starlightLinksValidatorPlugin()],
+    }),
+  ],
 });
