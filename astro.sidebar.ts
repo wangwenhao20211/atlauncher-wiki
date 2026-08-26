2b9d7915548e58784c32f923dd3373199d62a39b
wangwenhao20211 <159092574+wangwenhao20211@users.noreply.github.com>
2026-08-27 07:17:35 +0800

    更新 astro.sidebar.ts

--- astro.sidebar.ts
+++ astro.sidebar.ts
@@ -1,26 +1,14 @@
 export const sidebar = [
   {
-    label: '🎮 Wwhgames',
-    translations: { en: '🎮 Wwhgames' },
-    items: [
-      {
-        label: 'Wwhgames',
-        translations: { en: 'Wwhgames' },
-        autogenerate: { directory: 'wwhgames' },
-        collapsed: true,
-      },
-    ],
+    label: 'Wwhgames',
+    translations: { en: 'Wwhgames' },
+    autogenerate: { directory: 'wwhgames' },
+    collapsed: true,
   },
   {
-    label: '📺 B站UP',
-    translations: { en: '📺 Bilibili Creators' },
-    items: [
-      {
-        label: 'B站UP',
-        translations: { en: 'Bilibili Creators' },
-        autogenerate: { directory: 'bilibili_up' },
-        collapsed: true,
-      },
-    ],
+    label: 'B站UP',
+    translations: { en: 'Bilibili Creators' },
+    autogenerate: { directory: 'bilibili_up' },
+    collapsed: true,
   },
 ];
