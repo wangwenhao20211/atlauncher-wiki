62c3ec873b9478892af0e64d0a23714f18942e6c
wangwenhao20211 <159092574+wangwenhao20211@users.noreply.github.com>
2026-08-28 06:52:25 +0800

    Update astro.head.ts

--- astro.head.ts
+++ astro.head.ts
@@ -1,6 +1,10 @@
-import type { StarlightConfig } from '@astrojs/starlight';
-
-export const head: StarlightConfig['head'] = [
-  { tag: 'script', attrs: { type: 'module', src: '/scripts/l2d.js' } },
-  { tag: 'script', attrs: { type: 'module', src: '/scripts/reading-time.js' } },
-];
+export const head = [
+  {
+    tag: 'script',
+    attrs: { type: 'module', src: '/scripts/l2d.js' },
+  },
+  {
+    tag: 'script',
+    attrs: { type: 'module', src: '/scripts/reading-time.js' },
+  },
+] as const;
