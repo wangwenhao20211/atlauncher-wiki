37aed70b000d71197b57059f656cdfa91223eb2e
wangwenhao20211 <159092574+wangwenhao20211@users.noreply.github.com>
2026-08-28 06:33:48 +0800

    创建 astro.head.js

---
+++ astro.head.js
@@ -0,0 +1,10 @@
+export const head = [
+  {
+    tag: 'script',
+    attrs: { type: 'module', src: '/scripts/l2d.js' },
+  },
+  {
+    tag: 'script',
+    attrs: { type: 'module', src: '/scripts/reading-time.js' },
+  },
+];
